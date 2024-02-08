b81bc11366640d157bec813a14e08ab01f181bdd
Jaime Fernández <Jaime.fe.jb@gmail.com>
2024-02-08 18:24:41 +0100

    test

--- tdv/logger_settup.py
+++ tdv/logger_settup.py
@@ -6,3 +6,5 @@ LOG_FORMAT = "%(message)s"
 logging.basicConfig(filename='app.log', filemode='w', format=LOG_FORMAT, level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 tdv_logger = logging.getLogger(__name__)
+
+# Test
